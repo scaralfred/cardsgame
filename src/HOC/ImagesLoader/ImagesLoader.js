340794f5ebe6b8797fbfdec2653e91a6017fe679
scsaralfred <alfredoscarano87@gmail.com>
2018-06-23 00:46:24 +0800

    Preloaded imgaes with a preloader component nested into the main App.js component

--- src/HOC/ImagesLoader/ImagesLoader.js
+++ src/HOC/ImagesLoader/ImagesLoader.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
-
-const card1 = require("../../assets/cards/school/hoops.png");
-const card2 = require("../../assets/cards/school/box.png");
-const card3 = require("../../assets/cards/school/balls.png");
-const card4 = require("../../assets/cards/school/books.png");
-const card5 = require("../../assets/cards/school/jungle-gym.png");
-const card6 = require("../../assets/cards/school/markers.png");
-const card7 = require("../../assets/cards/school/scissors.png");
-const card8 = require("../../assets/cards/school/shelves.png");
+import { connect } from 'react-redux';
 
 class ImagesLoader extends Component {
     render() {
+
+    const cardsImages = Object.keys(this.props.cat)
+        .map((el)=>{ return Object.keys(this.props.cat[el])
+        .map((item) => { return Object.values(this.props.cat[el][item])
+        .map((obj) => { return <img src={require(`../../assets/cards/${item}/${obj}.png`)} /> 
+                })
+            })
+        });
+
         return (
             <div id="preload" style={{display: "none"}}>
-                <img src={card1} />
-                <img src={card2} />
-                <img src={card3} />
-                <img src={card4} />
-                <img src={card5} />
-                <img src={card6} />
-                <img src={card7} />
-                <img src={card8} />
+               {cardsImages}
             </div>
         );
     }
 }
 
-export default ImagesLoader;
+const mapStateToProps = state => {
+    return {
+        cat: state.categories.categories
+    }
+};
+
+export default connect(mapStateToProps, null)(ImagesLoader);
