1b74a8f997c23bd425b68531fd7e9d3e2c9ae838
scsaralfred <alfredoscarano87@gmail.com>
2018-06-25 21:00:38 +0800

    Adding erver

--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+}
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
- res.sendFile(path.join(__dirname +'/../build/index.html'));
-});
- 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'build')));
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
 app.listen(port);
 
 console.log(`Server started at port ${port}`);
