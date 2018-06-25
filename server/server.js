97536bde56eabc8b30eda9c6cca8decc71986ecc
scsaralfred <alfredoscarano87@gmail.com>
2018-06-25 21:09:20 +0800

    ff

--- server/server.js
+++ server/server.js
@@ -8,11 +8,9 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'build')));
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname+'/build/index.html'));
+});
 
 app.listen(port);
 
