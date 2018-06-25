f4395bd1a80353c2659635665341ce1c0070fac1
scsaralfred <alfredoscarano87@gmail.com>
2018-06-25 20:30:26 +0800

    Adding server

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, '../build')));
 //app.get('*', (req, res) => {
 //  res.sendFile(path.join(__dirname+'/clienild/index.html'));
 //});
-
+ 
 const port = process.env.PORT || 3000;
 app.listen(port);
 
